810ad663e5e2fc9001ea72fcc8b9402fbf96c53b
Bahman Dorand <bahman927@gmail.com>
2025-10-05 22:11:20 -0400

    update utils.s3_utils

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Photo, Category
+from .utils.s3_utils import get_signed_url
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,10 +14,15 @@ class PhotoSerializer(serializers.ModelSerializer):
         source="category",
         write_only=True
     )
-    image = serializers.ImageField(use_url=True)
+    # image = serializers.ImageField(use_url=True)
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Photo
         fields = "__all__"
 
-  
+    def get_image(self, obj):
+        request = self.context.get("request")
+        if obj.image:
+            return obj.image.url if obj.image.url.startswith("http") else request.build_absolute_uri(obj.image.url)
+        return None
---
+++ api/utils/s3_utils.py
@@ -0,0 +1,17 @@
+import boto3
+from django.conf import settings
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    region_name=settings.AWS_S3_REGION_NAME,
+)
+
+def get_signed_url(key):
+    """Generate a temporary S3 signed URL for private files."""
+    return s3.generate_presigned_url(
+        "get_object",
+        Params={"Bucket": settings.AWS_STORAGE_BUCKET_NAME, "Key": key},
+        ExpiresIn=3600  # URL valid for 1 hour
+    )
