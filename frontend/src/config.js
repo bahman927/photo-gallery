9a09aeca119af4d7541e9ec3a7541b4db8a305f2
Bahman Dorand <bahman927@gmail.com>
2025-10-12 18:37:27 -0400

    updte config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,12 +1,12 @@
 let apiUrl;
 
-// if (import.meta.env.PROD) {
-//   apiUrl = import.meta.env.VITE_API_URL || "https://photo-gallery-c9s4.onrender.com";
-// } else {
-//   apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8000";
-// }
+if (import.meta.env.PROD) {
+  apiUrl = import.meta.env.VITE_API_URL || "https://photo-gallery-c9s4.onrender.com";
+} else {
+  apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8000";
+}
 
- apiUrl =   "http://localhost:8000";
+//  apiUrl =   "http://localhost:8000";
 
 console.log("Using API URL:", apiUrl);
 export default apiUrl;
