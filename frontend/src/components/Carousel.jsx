0150097c3f011084dbce4542e54d479463bcbd2d
Bahman Dorand <bahman927@gmail.com>
2025-11-02 01:25:59 -0400

    update carousel.jsx

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -30,18 +30,18 @@ const PhotoCarousel = () => {
       {/* Carousel Thumbnails */}
 <div className="relative flex justify-center mt-0">
   {/* This is your visible frame — 2/3 width of page */}
-  <div className="w-3/5 overflow-hidden scrollbar-hide">
+  {/* <div className="min-w-fit max-w-lvw overflow-hidden scrollbar-hide"> */}
+  <div className="scroll-container min-w-fit w-full overflow-x-scroll scrolling-touch scrollbar-hide">
   
     
     
     {/* This is the animated strip of photos */}
-    <div className="group inline-flex whitespace-nowrap  items-center gap-4 pr-2 animate-scroll-left ">
+    <div className=" group inline-flex whitespace-nowrap  items-center gap-4 pr-2 animate-scroll-left ">
       {photos.map((photo, index) => (
         <div
           key={photo.id}
           onClick={() => setCurrentIndex(index)}
-          // className="photo-card flex items-center justify-center h-[100px] w-[150px] rounded-md overflow-hidden transition-transform duration-300 hover:scale-110 hover:shadow-2xl cursor-pointer"
-          className="photo-card inline-block h-[100px] w-[150px] rounded-md overflow-hidden transition-transform duration-300 hover:scale-110 hover:shadow-2xl cursor-pointer"
+          className="photo-card inline-block h-[100px] w-[120px] rounded-md overflow-hidden transition-transform duration-300 hover:scale-110 hover:shadow-2xl cursor-pointer"
         >
           <img
             src={photo.image}
@@ -51,29 +51,11 @@ const PhotoCarousel = () => {
         </div>
       ))}
     </div>
+    
   </div>
 </div>
 
-
-      {/* Carousel Thumbnails */}
-      {/* <div className="group flex items-center gap-4  pr-2  animate-scroll-left motion-safe:animate-fade-in-up  ">
-        <div className="flex gap-4 w-2/3 overflow-hidden">
-          {photos.map((photo, index) => (
-            <div
-              key={photo.id}
-              onClick={() => setCurrentIndex(index)}
-              className="photo-card flex items-center justify-center h-[100px] w-[150px] rounded-md overflow-hidden transition-transform duration-300 hover:scale-110 hover:shadow-2xl cursor-pointer"
-            >
-              <img
-                src={photo.image}
-                alt={photo.title || 'Photo'}
-                className="w-full h-full object-cover"
-              />
-            </div>
-          ))}
-        </div>
-      </div> */}
-    </div>
+</div>
 
 );
 
